a47c6fb3ac70ba7701fae9e7f2415f057da42c4e
Guilherme Silva <guinovembro43@gmail.com>
2025-03-03 21:30:16 -0300

    feat: Adding hot reload and creating board

--- Dockerfile
+++ Dockerfile
@@ -10,4 +10,4 @@ COPY . .
 
 EXPOSE 8080
 
-CMD ["npm", "run", "serve"]
+CMD ["npm", "run", "serve", "--public 0.0.0.0:8080"]
---
+++ src/components/ChessBoard.vue
@@ -0,0 +1,36 @@
+<template>
+    <section id="board">
+        <div class="square" v-for="i in 8" :key="i">
+            <div class="piece" v-for="j in 8" :key="j" :class="i % 2 === j % 2 ? 'white' : 'black'">
+    
+            </div>
+        </div>
+    </section>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+    #board {
+        display: block;
+        border: 1px solid black;
+        aspect-ratio: 1;
+        margin: 40px;
+    }
+
+    .square {
+        display: grid;
+        grid-template-columns: repeat(8, 1fr);
+        width: 100%;
+        height: calc(100% / 8);
+    }
+
+    .black {
+        background-color: #8B4513;
+    }
+
+    .white {
+        background-color: #FFDAB9;
+    }
+</style>
